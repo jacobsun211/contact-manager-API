9608954f022a8b42a9728ae734d3ca12bde69be8
jacob sun <jacobsun211@gmail.com>
2025-12-23 10:46:06 +0200

    compose and docker build up

---
+++ sql/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT, AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    phone_number VARCHAR(20) NOT NULL,UNIQUE
+);
